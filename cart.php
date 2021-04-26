84d395da06161e8167e0ec8f3a91fdb3ff20f31e
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2021-04-26 16:56:11 +0200

    Display contents of the cart

--- cart.php
+++ cart.php
@@ -1,6 +1,27 @@
 <?php
+	require_once($_SERVER["DOCUMENT_ROOT"]."/helpers/sessioncheck.php");
 	require_once($_SERVER["DOCUMENT_ROOT"]."/helpers/template.php");
+	require_once($_SERVER["DOCUMENT_ROOT"]."/helpers/dbhandler.php");
 	
-	$template = new Template($_SERVER["DOCUMENT_ROOT"]."/templates/cart.html");
+	$dbhandler = new DBHandler();
+
+	$cards = "";
+	$total = 0;
+	foreach (array_keys($_SESSION["cart"]) as $item) {
+		$details = $dbhandler->product_details($item)[0];
+		$subtotal = $_SESSION["cart"][$item] * $details["price"];
+		$total += $subtotal;
+		$context = array(
+			"name" => $details["displayname"],
+			"slug" => $item,
+			"description" => $details["description"],
+			"count" => $_SESSION["cart"][$item],
+			"subtotal" => number_format($subtotal, 0, ",", " "),
+		);
+		$card = new Template($_SERVER["DOCUMENT_ROOT"]."/templates/cart-card.html", $context);
+		$cards .= $card;
+	}
+	// die;
+	$template = new Template($_SERVER["DOCUMENT_ROOT"]."/templates/cart.html", array("cards" => $cards, "total" => number_format($total, 0, ",", " ")));
 	$template->render();
 ?>
