bcced628bad0bc66a2166f7ef24d57326dbd5869
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2021-05-11 11:51:05 +0200

    Give 401 without login instead of 403

--- error.php
+++ error.php
@@ -3,6 +3,7 @@
 
 	$response_codes = array(
 		400 => "Hibás kérés",
+		401 => "Bejelentkezés szükséges",
 		403 => "Hozzáférés megtagadva",
 		404 => "Az oldal nem található",
 		500 => "Hiba történt a szerveren"
@@ -13,10 +14,6 @@
 		"message" => $response_codes[http_response_code()],
 		"info" => ""
 	);
-
-	if(http_response_code() === 403) {
-		$ctx["info"] = "Az oldal megtekintéséhez meg kell adnia az adatait.";
-	}
 
 	$template = new Template($_SERVER['DOCUMENT_ROOT']."/templates/error.html", $ctx);
 	$template->render();
--- helpers/sessioncheck.php
+++ helpers/sessioncheck.php
@@ -3,7 +3,7 @@
 	$required_fields = array("fullname", "email", "phone", "postcode", "city", "address");
 	foreach($required_fields as $field) {
 		if(!isset($_SESSION[$field]) || empty($_SESSION[$field])) {
-			http_response_code(403);
+			http_response_code(401);
 			require_once($_SERVER["DOCUMENT_ROOT"]."/error.php");
 			exit;
 		}
